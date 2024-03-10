033c95f1ebf8a40d1a2da342f19979e3b723c674
Raj-Vaghela <rajvaghela012@gmail.com>
2024-03-10 15:02:53 +0000

    Check

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+df = pd.read_csv('diabetic_data.csv')
+print(df.head())
+print(df.describe())
+print(df.isnull().sum())
+print(df.isnull()
+      .value_counts())
